3876e9460e1add3db7743e0392d5dc36f05c3bb4
apan64 <andrew.pan@students.olin.edu>
2016-03-24 14:08:43 -0400

    Delete env.sh

--- env.sh
+++
@@ -1,4 +0,0 @@
-source venv27/bin/activate
-export JUPYTER_CONFIG_DIR=/home/apan/dev/notebook/jupyter_config
-export JUPYTER_DATA_DIR=/home/apan/dev/notebook/jupyter_data
-export IPYTHONDIR=/home/apan/dev/notebook/ipython_config
